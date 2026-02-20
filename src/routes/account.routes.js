5e4ce568e973c52c2d3e1cef2bf9f48622c6a6d3
Your Name <mr.shikhar100@gmail.com>
2026-02-20 23:57:40 +0530

    day-9 some modification in account controller and account routes

--- src/routes/account.routes.js
+++ src/routes/account.routes.js
@@ -14,6 +14,19 @@ const router = express.Router()
  */
 router.post("/",authMiddleware.authMiddleware,accountController.createAccountController)
 
+/***
+ * -Get /api/accounts
+ * - Get all accounts of the logged in user
+ * - Protected Route
+ */
+router.get("/",authMiddleware.authMiddleware,accountController.getUserAccountController)
+
+
+/**
+ * - GET /api/accounts/balance/:accountId
+ */
+router.get("/balance/:accountId",authMiddleware.authMiddleware,accountController.getAccountBalanceController)
+
 
 
 module.exports = router
